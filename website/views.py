9037cba4b9d657ff5c1b5936a1bf8ff63ff8a10d
kepler0101 <115867373+kepler0101@users.noreply.github.com>
2022-12-18 17:40:09 +0400

    Add files via upload

---
+++ website/views.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
+from sqlalchemy import engine
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Слишком короткое объявление!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Объявление добавлено!', category='success')
+
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
+
+@views.route('/all-notes')
+def show_all_notes(): 
+    return render_template("all_notes.html", user=current_user)
